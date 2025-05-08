6ece7372cd6793a8f747879985a8dd1f07f33fdf
Marc Mendoza <777marc@gmail.com>
2025-05-08 14:37:01 -0400

    misc

--- bin/ec2-01.ts
+++ bin/ec2-01.ts
@@ -3,4 +3,6 @@ import * as cdk from "aws-cdk-lib";
 import { Ec201Stack } from "../lib/ec2-01-stack";
 
 const app = new cdk.App();
-new Ec201Stack(app, "Ec201Stack");
+new Ec201Stack(app, "Ec201Stack", {
+  env: { account: "388414971737", region: "us-east-1" },
+});
--- lib/ec2-01-stack.ts
+++ lib/ec2-01-stack.ts
@@ -1,8 +1,28 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import {
+  Instance,
+  Vpc,
+  MachineImage,
+  InstanceType,
+  InstanceClass,
+  InstanceSize,
+} from "aws-cdk-lib/aws-ec2";
 
 export class Ec201Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = Vpc.fromLookup(this, "MyDefaultVpc", {
+      isDefault: true,
+    });
+
+    const instance = new Instance(this, "MyEc2Instance", {
+      vpc,
+      machineImage: MachineImage.genericLinux({
+        "us-east-1": "ami-084568db4383264d4",
+      }),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    });
   }
 }
